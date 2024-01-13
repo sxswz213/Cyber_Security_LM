c78fe0624a3dd2c25a12e8d37bec969f99e23fd6
sxswz213 <115558821+sxswz213@users.noreply.github.com>
2024-01-13 14:57:12 +0800

    Create LLM.py

---
+++ LLM.py
@@ -0,0 +1,37 @@
+from langchain.llms.base import LLM
+from typing import Any, List, Optional
+from langchain.callbacks.manager import CallbackManagerForLLMRun
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+
+class InternLM_LLM(LLM):
+    # 基于本地 InternLM 自定义 LLM 类
+    tokenizer : AutoTokenizer = None
+    model: AutoModelForCausalLM = None
+
+    def __init__(self, model_path :str):
+        # model_path: InternLM 模型路径
+        # 从本地初始化模型
+        super().__init__()
+        print("正在加载模型...")
+        self.tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+        self.model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True).to(torch.bfloat16).cuda()
+        self.model = self.model.eval()
+        print("完成模型的加载")
+
+    def _call(self, prompt : str, stop: Optional[List[str]] = None,
+                run_manager: Optional[CallbackManagerForLLMRun] = None,
+                **kwargs: Any):
+        # 重写调用函数
+        system_prompt = """You are an AI assistant whose name is Cyber_Security_LM (网络空间安全小助手).
+        - Cyber_Security_LM (网络空间安全小助手) is a conversational language model that is developed by sxswz. It is designed to be helpful, honest, and harmless. It is designed to help people solve the security problems in cyberspace.
+        - Cyber_Security_LM (网络空间安全小助手) can understand and communicate fluently in the language chosen by the user such as 中文 and English.
+        """
+        
+        messages = [(system_prompt, '')]
+        response, history = self.model.chat(self.tokenizer, prompt , history=messages)
+        return response
+        
+    @property
+    def _llm_type(self) -> str:
+        return "InternLM"
